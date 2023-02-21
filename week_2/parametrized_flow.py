2e3384342df0d2b2a251652e5f36d2dae561b755
alewieland <alexander.wieland.aw@gmail.com>
2023-02-21 08:52:40 +0100

    Update parametrized_flow.py

--- week_2/parametrized_flow.py
+++ week_2/parametrized_flow.py
@@ -70,6 +70,6 @@ def etl_parent_flow(months: list[int] = [11], year: int = 2020, color: str = 'gr
 
 if __name__ == '__main__':
     color = 'yellow'
-    months = [1,2,3,4,5,6,7,8,9,10,11,12]
+    months = [10,11,12]
     year = 2019
     etl_parent_flow(months, year, color)
